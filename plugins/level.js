17c0cdd68de61c6052cf867f1599ec1c1c4086b5
botat12120 <witcher12120@gmail.com>
2024-09-25 13:34:41 +0300

    Update level.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/level.js
+++ plugins/level.js
@@ -2,8 +2,10 @@ import { canLevelUp, xpRange } from '../lib/levelling.js'
 import { levelup } from '../lib/canvas.js'
 
 let handler = async (m, { conn }) => {
-	let name = conn.getName(m.sender)
+    let name = conn.getName(m.sender)
     let user = global.db.data.users[m.sender]
+    
+    // التحقق من إمكانية رفع المستوى
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
         throw `
@@ -16,10 +18,14 @@ let handler = async (m, { conn }) => {
 انت تحتاج الي *${max - user.exp}* *XP* لرفع مستواك
 `.trim()
     }
+
     let before = user.level * 1
+
+    // زيادة المستوى حتى الوصول إلى الحد الأعلى بناءً على XP
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+
     if (before !== user.level) {
-        let teks = `🎊 مبروك لدخواك المستوى الجديد  ${conn.getName(m.sender)}    المستوي:`
+        let teks = `🎊 مبروك لدخواك المستوى الجديد ${name}! المستوي:`
         let str = `
 ┌─⊷ *المستوي*
 ▢ المستوي السابق : *${before}*
@@ -28,10 +34,14 @@ let handler = async (m, { conn }) => {
 
 *_كلما تفاعلت مع البوت ارتفع مستواك_*
 `.trim()
+
         try {
+            // إنشاء صورة المستوى الجديد باستخدام canvas
             const img = await levelup(teks, user.level)
+            // إرسال الصورة مع النص التوضيحي
             conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
         } catch (e) {
+            // في حالة وجود خطأ أثناء توليد الصورة، إرسال النص فقط
             m.reply(str)
         }
     }
@@ -40,6 +50,7 @@ let handler = async (m, { conn }) => {
 handler.help = ['levelup']
 handler.tags = ['xp']
 
-handler.command = ['لفل', 'lvl', 'levelup', 'مستواي', 'مستوا'] 
+// الأوامر التي يمكن أن تستدعي هذا المعالج
+handler.command = ['لفل', 'lvl', 'levelup', 'مستواي', 'مستوا']
 
 export default handler
