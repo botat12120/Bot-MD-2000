011c7bc641021056ffc8f320941fae62fceab88b
botat12120 <witcher12120@gmail.com>
2024-09-26 20:43:19 +0300

    Update _antilink.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,13 +1,5 @@
-// TheMystic-Bot-MD@BrunoSobrino - _antilink.js
-
-  
 const linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
 export async function before(m, {conn, isAdmin, isBotAdmin}) {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins._antilink
-
   if (m.isBaileys && m.fromMe) {
     return !0;
   }
@@ -19,19 +11,19 @@ export async function before(m, {conn, isAdmin, isBotAdmin}) {
   const user = `@${m.sender.split`@`[0]}`;
   const isGroupLink = linkRegex.exec(m.text);
   const grupo = `https://chat.whatsapp.com`;
-  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply(tradutor.texto1);
+  if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*تم تفعيل مضاد الروابط من طرف المشرفين*');
   if (chat.antiLink && isGroupLink && !isAdmin) {
     if (isBotAdmin) {
       const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
       if (m.text.includes(linkThisGroup)) return !0;
     }
-    await this.sendMessage(m.chat, {text: tradutor.texto2, mentions: [m.sender]}, {quoted: m});
-    if (!isBotAdmin) return m.reply(tradutor.texto3);
+    await this.sendMessage(m.chat, {text: `*「 ⚒️ نظام الحماية ⚒️ 」*\n*تم رصد رابط واتس من ${user} ...!!*`, mentions: [m.sender]}, {quoted: m});
+    if (!isBotAdmin) return m.reply('*البوت مامعه إشراف عان يطرد!*');
     if (isBotAdmin && bot.restrict) {
       await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
       const responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
       if (responseb[0].status === '404') return;
-    } else if (!bot.restrict) return m.reply(tradutor.texto4);
+    } else if (!bot.restrict) return m.reply('*المرجوا من مؤسس المجموعة او المطور ان يفعلوا الريس*');
   }
   return !0;
 }
