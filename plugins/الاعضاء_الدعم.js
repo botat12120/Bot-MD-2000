c652d426d86417484f65db53ae0446f33d03665b
botat12120 <witcher12120@gmail.com>
2024-09-27 03:44:27 +0300

    Create الاعضاء_الدعم.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

---
+++ plugins/الاعضاء_الدعم.js
@@ -0,0 +1,17 @@
+let handler = async m => m.reply(`
+
+≡  *ROB ↳🔰↲ 𝐁𝐎𝐓*
+
+_GROUP:_
+─────────────
+▢ انضم إلى مجموعة البوت *العامة*
+https://chat.whatsapp.com/Ck3mQdmm0Ty3FMrJSNsG90
+─────────────
+
+
+`.trim())
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['group', 'support','الدعم','دعم']
+
+export default handler
