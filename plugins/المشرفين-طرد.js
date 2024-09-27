e7785c8cd4c8218ffb851080575491059953b5b8
botat12120 <witcher12120@gmail.com>
2024-09-27 15:17:32 +0300

    Update المشرفين-طرد.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/المشرفين-طرد.js
+++ plugins/المشرفين-طرد.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
 let kickte = `*مــنشـن الـشـخص !*`
-if (!global.db.data.settings[conn.user.jid].restrict) throw `${tradutor.texto1[0]} (*_restrict_*), ${tradutor.texto1[1]}`;
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
+  if (!global.db.data.settings[conn.user.jid].restrict) throw `${tradutor.texto1[0]} (*_restrict_*), ${tradutor.texto1[1]}`;
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
