9440484728e6ca11de26c6ae5da0d60e77a69d57
botat12120 <witcher12120@gmail.com>
2024-09-28 14:11:29 +0300

    Update ادوات-جودة صوره.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/ادوات-جودة صوره.js
+++ plugins/ادوات-جودة صوره.js
@@ -1,29 +1,22 @@
 import FormData from "form-data";
 import Jimp from "jimp";
-
-
 const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.herramientas_hd
-
  try {    
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) throw `${tradutor.texto1} ${usedPrefix + command}*`;
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `${tradutor.texto2[0]} (${mime}) ${tradutor.texto2[1]}`;
-  m.reply(tradutor.texto3);
+  if (!mime) throw `╰⊱❗️⊱ *عمل سيء* ⊱❗️⊱╮\n\nقم بالرد علي الصوره ${usedPrefix + command}`;
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `╰⊱⚠️⊱ *⌞🔰┊ 𝑹𝑶𝑩 𝑩𝑶𝑻 ┊🔰⌝* ⊱⚠️⊱╮\n\nEL FORMATO DEL ARCHIVO (${mime}) NO ES COMPATIBLE, ENVÍA O RESPONDE A UNA FOTO`;
+  m.reply("*جـاري الـتـحـسـين...🐈*");
   let img = await q.download?.();
   let pr = await remini(img, "enhance");
   conn.sendMessage(m.chat, {image: pr}, {quoted: m});
  } catch {
-  throw tradutor.texto4;
+  throw "قم بالرد على صورة";
  }
 };
-handler.help = ["جودة", "hd", "enhance"];
+handler.help = ["remini", "hd", "enhance"];
 handler.tags = ["ai", "tools"];
-handler.command = ["جودة", "hd", "enhance"];
+handler.command = ["جوده", "جودة", "تحسين"];
 export default handler;
 
 async function remini(imageData, operation) {
