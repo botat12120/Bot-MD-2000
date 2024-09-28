7988cc8b227b88a5159746c63be2c27109d72293
botat12120 <witcher12120@gmail.com>
2024-09-28 18:07:37 +0300

    Update المجموعات_انذار2.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/المجموعات_انذار2.js
+++ plugins/المجموعات_انذار2.js
@@ -1,26 +1,24 @@
-let handler = async (m, { conn, args, groupMetadata }) => {
-  let who
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-  else who = m.chat
-  if (!who) throw `✳️ اعمل منشن لي الشخص`
-  if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
-  let warn = global.db.data.users[who].warn
-  if (warn > 0) {
-    global.db.data.users[who].warn -= 1
-    m.reply(`⚠️ *DELWARN*
-         
-▢ Warns: *-1*
-▢ Warns total: *${warn - 1}*`)
-    m.reply(`✳️ تم ازالة الانذار بنجاح *${warn - 1}*`, who)
-  } else if (warn == 0) {
-    m.reply('✳️ المستخدم ليس لديه انذارات')
-  }
-}
-handler.help = ['delwarn @user']
-handler.tags = ['group']
-handler.command = ['رفع_الانذار', 'unwarn']
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
+const handler = async (m, {conn, text, command, usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins.gc_unwarn
 
-export default handler
+  const pp = './src/assets/images/menu/main/warn.jpg';
+  let who;
+  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text;
+  else who = m.chat;
+  const user = global.db.data.users[who];
+  const bot = global.db.data.settings[conn.user.jid] || {};
+  const warntext = `${tradutor.texto1}\n*${usedPrefix + command} @${global.suittag}*`;
+  if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
+  if (m.mentionedJid.includes(conn.user.jid)) return;
+  if (user.warn == 0) throw tradutor.texto2;
+  user.warn -= 1;
+  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`}${tradutor.texto3} ${user.warn}/3*`, null, {mentions: [who]});
+};
+handler.command = /^(unwarn|delwarn|deladvertir|deladvertencia|delwarning)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+export default handler;
