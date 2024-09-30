7de4356bf26759539dce3ecdfbf9fa7010dc215c
botat12120 <witcher12120@gmail.com>
2024-09-30 21:07:59 +0300

    Update المجموعات_انذار3.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/المجموعات_انذار3.js
+++ plugins/المجموعات_انذار3.js
@@ -1,24 +1,24 @@
-let handler = async (m, { conn, args, groupMetadata }) => {
-  let who = m.quoted
-    ? m.quoted.sender
-    : m.mentionedJid && m.mentionedJid[0]
-      ? m.mentionedJid[0]
-      : m.fromMe
-        ? conn.user.jid
-        : m.sender
-  if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
-  let warn = global.db.data.users[who].warn
-  let name = conn.getName(who)
-  m.reply(`
- *الانذارات*
-
-▢ *الاسم :* ${name} 
-▢ *الانذارات :* ${warn}`)
+let handler = async (m, { conn, groupMetadata }) => {
+  let users = global.db.data.users // جلب جميع المستخدمين من قاعدة البيانات
+  let warningList = []
+  
+  for (let jid in users) {
+    if (users[jid].warn > 0) { // التحقق إذا كان المستخدم لديه إنذارات
+      let name = conn.getName(jid) || 'Unknown' // جلب الاسم أو استخدام 'Unknown' في حالة عدم وجوده
+      warningList.push(`▢ *الاسم:* ${name} \n▢ *الإنذارات:* ${users[jid].warn}\n`)
+    }
+  }
+  
+  if (warningList.length === 0) {
+    m.reply('✳️ لا يوجد أي إنذارات مسجلة.')
+  } else {
+    m.reply(`*قائمة الإنذارات:*\n\n${warningList.join('\n')}`)
+  }
 }
 
-handler.help = ['warns']
+handler.help = ['warnlist']
 handler.tags = ['group']
-handler.command = ['الانذارات']
+handler.command = ['الانذارات', 'warnlist'] // الأمر الذي يقوم بعرض القائمة
 handler.group = true
 
 export default handler
