97200b67d47a113830325893f37c002ce937f130
botat12120 <witcher12120@gmail.com>
2024-10-21 20:28:05 +0300

    Update 💤-reporte.js
    Signed-off-by: botat12120 <witcher12120@gmail.com>

--- plugins/💤-reporte.js
+++ plugins/💤-reporte.js
@@ -3,8 +3,8 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `*البلاغ لازم يكون فيه أكثر من ١٠ حروف!*`;
   if (text.length > 1000) throw `*الحد الأقصى للبلاغ ١٠٠٠ حرف!*`;
   const teks = `*❒ بلاغ جديد ❒*\n*≡ صاحب البلاغ :* ${m.sender.split`@`[0]}\n*≡ بلاغه:* *${text}*`;
-  conn.reply('966577896920@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('212703388528@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('966551609722@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('966553211410@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`> *تم ارسال البلاغ*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
